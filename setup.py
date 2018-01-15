ec57abfb00b5990850e033dffdce45a5976d5328
David Tyler <david.tyler@metaswitch.com>
2018-01-15 17:09:50 +0000

    No fstrings on python 3.5 so minimum version is now 3.6

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     url='https://github.com/DaveTCode/confluence-python-lib',
     keywords=['confluence'],
     classifiers=[],
+    python_requires='>=3.6',
     setup_requires=['pytest-runner'],
     install_requires=['requests >= 2.18.4, < 3.0.0a0'],
     tests_require=['pytest >= 3.0.7, < 4.0.0a0', 'pytest-cov >= 2.5.0, < 3.0.0a0']
