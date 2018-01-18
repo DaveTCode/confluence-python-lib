26149476551900e90467062be5c3dc9ad2c79e13
David Tyler <david.tyler@metaswitch.com>
2018-01-18 19:35:12 +0000

    Better pypi packaging information including a long description from the README.md

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
+from codecs import open
+from os import path
 from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='confluence-rest-library',
-    packages=find_packages(),
-    version='0.2.2',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    version='0.2.3',
     description='A simple wrapper around the Confluence REST API.',
+    long_description=long_description,
     author='David Tyler',
     author_email='davet.code@gmail.com',
     url='https://github.com/DaveTCode/confluence-python-lib',
     keywords=['confluence'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
     python_requires='>=3.6',
     setup_requires=['pytest-runner'],
     install_requires=['requests >= 2.18.4, < 3.0.0a0'],
