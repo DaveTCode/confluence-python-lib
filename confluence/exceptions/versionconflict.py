ba4a866bbe12840b19bedfebbec1cf2558601427
David Tyler <davet.code@gmail.com>
2018-03-10 15:30:06 +0000

    Fix line too long issue

--- confluence/exceptions/versionconflict.py
+++ confluence/exceptions/versionconflict.py
@@ -14,5 +14,6 @@ class ConfluenceVersionConflict(ConfluenceError):
     """
     def __init__(self, path, params, response):
         # type: (str, Dict[str, str], requests.Response) -> None
-        msg = 'The given version does not match the expected next version. This is likely because someone else has made a change to the resource at path {}'.format(path)
+        msg = 'The given version does not match the expected next version. This is likely because someone else has ' \
+              'made a change to the resource at path {}'.format(path)
         super(ConfluenceVersionConflict, self).__init__(path, params, response, msg)
