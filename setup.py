80cd64098dddc41702d2079e2e1c9ab3cc82b49f
David Tyler <davet.code@gmail.com>
2018-03-10 15:22:24 +0000

    0.10.1 - Version which includes readthedocs

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(readme, encoding='utf-8') as f:
 setup(
     name='confluence-rest-library',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    version='0.9.0',
+    version='0.10.1',
     description='A simple wrapper around the Confluence REST API.',
     long_description=long_description,
     author='David Tyler',
