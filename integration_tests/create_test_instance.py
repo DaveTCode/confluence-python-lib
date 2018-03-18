9554466ae6583b0c9bef75b1e6d023f52bb730bf
David Tyler <davet.code@gmail.com>
2018-03-18 14:04:24 +0000

    Think it might be hanging waiting for a response on the mailing list command

--- integration_tests/create_test_instance.py
+++ integration_tests/create_test_instance.py
@@ -5,6 +5,8 @@ cmd = ['atlas-run-standalone', '--product', 'confluence', '--version', '6.6.0', 
 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 for line in p.stdout:
     if line.startswith('Would you like to subscribe to the Atlassian developer mailing list'.encode()):
-        p.stdin.write('n\n'.encode())
+        print('Found mailing list line')
+        p.stdin.writelines('n'.encode())
     elif line.startswith('[INFO] Type Ctrl-C to exit'.encode()):
+        print('Found exit line')
         break
