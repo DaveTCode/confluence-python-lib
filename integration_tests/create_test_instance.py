d96f5a97153c4ffdaa40d16699258e8250632d76
David Tyler <davet.code@gmail.com>
2018-03-18 14:30:02 +0000

    Added print statements to see what's going wrong in the create_test_instance script

--- integration_tests/create_test_instance.py
+++ integration_tests/create_test_instance.py
@@ -1,12 +1,25 @@
+import argparse
 import subprocess
 
-cmd = ['atlas-run-standalone', '--product', 'confluence', '--version', '6.6.0', '--server', 'localhost']
 
-p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-for line in p.stdout:
-    if line.startswith('Would you like to subscribe to the Atlassian developer mailing list'.encode()):
-        print('Found mailing list line')
-        p.stdin.writelines('n'.encode())
-    elif line.startswith('[INFO] Type Ctrl-C to exit'.encode()):
-        print('Found exit line')
-        break
+def create_detached_server(version):
+    # type: (str) -> None
+    cmd = ['atlas-run-standalone', '--product', 'confluence', '--version', version, '--server', 'localhost']
+
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in p.stdout:
+        print(line)
+        if line.startswith('Would you like to subscribe to the Atlassian developer mailing list'.encode()):
+            print('Found mailing list line')
+            p.stdin.writelines('n'.encode())
+        elif line.startswith('[INFO] Type Ctrl-C to exit'.encode()):
+            print('Found exit line')
+            break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('version', default='6.6.0')
+    args = parser.parse_args()
+
+    create_detached_server(args.version)
