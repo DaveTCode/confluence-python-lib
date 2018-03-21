3326809c0ce32e115c9a636d03b9ed5f67e29bb0
David Tyler <david.tyler@metaswitch.com>
2018-03-21 15:49:09 +0000

    Go back to not using atlas-version to bring this branch back in line with master

--- integration_tests/config.py
+++ integration_tests/config.py
@@ -1,5 +1,4 @@
 from confluence.client import Confluence
-import logging
 import os
 
 local_url = 'http://localhost:1990/confluence'
@@ -8,11 +7,8 @@ local_admin = ('admin', 'admin')
 
 def get_confluence_instance():
     # type: () -> Confluence
-    print(os.system('curl --fail http://localhost:1990/confluence/rest/api/space'))
     user = os.environ.get('ATLASSIAN_CLOUD_USER')
     password = os.environ.get('ATLASSIAN_CLOUD_PASSWORD')
     url = os.environ.get('ATLASSIAN_CLOUD_URL')
-
-    logging.info(url)  # This isn't really secret, just testing env variable retrieval
 
     return Confluence(url, (user, password)) if user and password and url else Confluence(local_url, local_admin)
