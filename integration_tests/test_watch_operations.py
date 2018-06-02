6276daf2e35a059836292254a5fe3d282d0b38a2
David Tyler <davet.code@gmail.com>
2018-06-02 12:42:29 +0100

    Tweak test so that we verify all the space watch functions based on user_key as well as username

--- integration_tests/test_watch_operations.py
+++ integration_tests/test_watch_operations.py
@@ -49,10 +49,10 @@ def test_user_is_watching_space():
 
 
 def test_user_is_watching_space_by_key():
-    c.add_space_watch(space_key, username='admin')
     user = c.get_user(username='admin')
+    c.add_space_watch(space_key, user_key=user.user_key)
     assert c.is_user_watching_space(space_key, user_key=user.user_key)
-    c.remove_space_watch(space_key, username='admin')
+    c.remove_space_watch(space_key, user_key=user.user_key)
 
 
 def test_remove_space_watch_without_one():
