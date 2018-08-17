ecac857fe2dcf3c26b9664dd3ab564fc6f800111
David Tyler <david.tyler@metaswitch.com>
2018-08-17 16:55:09 +0100

    1.0.1 - Release commit

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(readme, encoding='utf-8') as f:
 setup(
     name='confluence-rest-library',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    version='1.0.0',
+    version='1.0.1',
     description='A simple wrapper around the Confluence REST API.',
     long_description=long_description,
     author='David Tyler',
@@ -23,7 +23,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     python_requires='>=2.7,!=3.0,!=3.1,!=3.2,!=3.3,!=3.4',
     setup_requires=['pytest-runner', 'pypandoc', 'typing', 'pycodestyle', 'bandit', 'mypy'],
