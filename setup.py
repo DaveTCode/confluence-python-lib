411220d611d1eaea92f073d82641421289014eda
David Tyler <david.tyler@metaswitch.com>
2019-10-02 13:37:15 +0100

    #24 - Stop including integration tests in built package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(readme, encoding='utf-8') as f:
 
 setup(
     name='confluence-rest-library',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'integration_tests']),
     version='2.0.0',
     description='A simple wrapper around the Confluence REST API.',
     long_description=long_description,
